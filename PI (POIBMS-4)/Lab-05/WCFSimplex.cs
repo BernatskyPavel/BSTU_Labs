4a89c996a6576fed93de37590a63de2a8b9a4ee7
Bernatsky Pavel <pasha.desh@yandex.by>
2021-10-17 01:26:09 +0300

    Added PI (POIBMS-4) Lab-05

---
+++ PI (POIBMS-4)/Lab-05/WCFSimplex.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Lab_05 {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class WCFSimplex : IWCFSimplex {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public string Concat(string s, double d)
+        {
+            return s + d;
+        }
+
+        public A Sum(A a1, A a2)
+        {
+            return a1 + a2;
+        }
+    }
+}
---
+++ PI (POIBMS-4)/Windows/Hosts/Lab-05-Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab_05;
+using System.ServiceModel;
+
+namespace Lab_05_Console {
+    class Program {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(Lab_05.WCFSimplex));
+            host.Open();
+            Console.WriteLine("Host opened!\nPress any key to stop...");
+            Console.Read();
+            host.Close();
+            Console.WriteLine("Host closed!");
+        }
+    }
+}
